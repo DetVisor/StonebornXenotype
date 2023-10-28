3b279842d7bd10b6b9e8a79bc6649e320e35ce56
Dexter Lowe <feldoh10@gmail.com>
2023-10-28 22:34:06 +0100

    Hediff Name update

--- 1.4/Source/StonebornXenotype/StonebornDefOf.cs
+++ 1.4/Source/StonebornXenotype/StonebornDefOf.cs
@@ -6,7 +6,7 @@ namespace StonebornXenotype;
 [DefOf]
 public static class StonebornDefOf
 {
-    public static HediffDef DV_MightyLiver;
+    public static HediffDef DV_MightyLiverHediff;
 
     static StonebornDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(StonebornDefOf));
 }
