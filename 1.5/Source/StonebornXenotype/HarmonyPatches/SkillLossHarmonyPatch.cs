53e7bfb48c543c7e698aa83d9b6f4088a1f677a5
DetVisor <113966499+DetVisor@users.noreply.github.com>
2024-03-29 17:17:19 +0100

    Update

---
+++ 1.5/Source/StonebornXenotype/HarmonyPatches/SkillLossHarmonyPatch.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+
+namespace StonebornXenotype.HarmonyPatches;
+
+[HarmonyPatch(typeof(SkillRecord), nameof(SkillRecord.Interval))]
+public static class SkillLossHarmonyPatch
+{
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> SkillLossHarmonyPatchTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+        bool patched = false;
+        foreach (CodeInstruction instruction in instructions)
+        {
+            yield return instruction;
+            if (instruction.opcode == OpCodes.Stloc_0 && !patched)
+            {
+                patched = true;
+                yield return new CodeInstruction(OpCodes.Ldloc_0);
+                yield return new CodeInstruction(OpCodes.Ldarg_0);
+                yield return CodeInstruction.Call(typeof(SkillLossHarmonyPatch), nameof(GetSkillLossFactor));
+                yield return new CodeInstruction(OpCodes.Mul);
+                yield return new CodeInstruction(OpCodes.Stloc_0);
+            }
+
+        }
+    }
+
+    public static float GetSkillLossFactor(SkillRecord skill) => StonebornMemoryGene.GetSkillLossFactorForPawn(skill.Pawn);
+}
