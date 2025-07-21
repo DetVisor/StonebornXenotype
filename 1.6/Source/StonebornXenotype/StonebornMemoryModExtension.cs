b0a795a6791579f21136db9bf5232656c2597a9f
DetVisor <det.visor@gmail.com>
2025-07-21 02:36:18 +0200

    Updating

--- 1.6/Source/StonebornXenotype/StonebornMemoryModExtension.cs
+++
@@ -1,9 +0,0 @@
-using Verse;
-
-namespace StonebornXenotype;
-
-public class StonebornMemoryModExtension : DefModExtension
-{
-    public float skillLossFactor = 1f;
-    public float thoughtDurationFactor = 1f;
-}
